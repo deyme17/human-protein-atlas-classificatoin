9c7b67226ef44d5bb73c53bbf25d8d7c0939e4e9
dmytro <dmytro19022006@gmail.com>
2026-04-21 01:37:40 +0300

    plots

---
+++ utils/plots.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+
+def visualize_training(history: dict[str, list[float]]) -> None:
+    fig, ax = plt.subplots(2, 1, figsize=(10, 6))
+    ax[0].plot(history['train_loss'], label='Train Loss')
+    ax[0].plot(history['valid_loss'], label='Valid Loss')
+    ax[0].set_title('Losses')
+    ax[0].legend()
+    ax[1].plot(history['f1_macro'], label='F1-Macro')
+    ax[1].plot(history['f1_micro'], label='F1-Micro')
+    ax[1].plot(history['f1_samples'], label='F1-Samples')
+    ax[1].set_title('F1 Scores')
+    ax[1].legend()
+    plt.tight_layout()
+    plt.show()
