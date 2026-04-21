101740b3de3475e921bb3f5b98bee8e3d613355c
dmytro <dmytro19022006@gmail.com>
2026-04-21 11:47:07 +0300

    optimizer factory

---
+++ optimizers/optimizer_factory.py
@@ -0,0 +1,34 @@
+import torch.optim as optim
+from config import TrainConfig
+
+
+def adam(parameters, lr=0.001, betas=(0.9, 0.999), weight_decay=0, amsgrad=False, **_):
+    if isinstance(betas, str):
+        betas = eval(betas)
+    return optim.Adam(parameters, lr=lr, betas=betas, weight_decay=weight_decay, amsgrad=amsgrad)
+
+
+def adamw(parameters, lr=0.001, betas=(0.9, 0.999), weight_decay=0, amsgrad=False, **_):
+    if isinstance(betas, str):
+        betas = eval(betas)
+    return optim.AdamW(parameters, lr=lr, betas=betas, weight_decay=weight_decay, amsgrad=amsgrad)
+
+
+def sgd(parameters, lr=0.001, momentum=0.9, weight_decay=0, nesterov=True, **_):
+    return optim.SGD(parameters, lr=lr, momentum=momentum, weight_decay=weight_decay, nesterov=nesterov)
+
+
+_OPTIMIZERS = {
+    "adam": adam,
+    "adamw": adamw,
+    "sgd": sgd,
+}
+
+
+def get_optimizer(parameters) -> optim.Optimizer:
+    cfg = TrainConfig.optimizer
+    name = cfg["name"].lower()
+    params = cfg.get("parameters", {})
+    if name not in _OPTIMIZERS:
+        raise ValueError(f"Unknown optimizer '{name}'. Available: {list(_OPTIMIZERS)}")
+    return _OPTIMIZERS[name](parameters, **params)
