c02ff8149bc95faeb53177dc80d91ee6ac3a6431
dmytro <dmytro19022006@gmail.com>
2026-04-23 19:06:28 +0300

    dont save pathes

--- dataset/load_save.py
+++ dataset/load_save.py
@@ -1,4 +1,5 @@
 from .dataset import ProteinDataset
+from config import PathConfig
 import torch
 from pathlib import Path
 
@@ -7,20 +8,21 @@ def save_dataset(ds: ProteinDataset, path: Path):
     state = {
         'image_ids': ds.image_ids,
         'labels': ds.labels,
-        'train_root': ds.train_root,
-        'external_root': ds.external_root,
         'transform': ds.transform 
     }
     torch.save(state, path)
 
 
-def load_dataset(path: Path, transform=None) -> ProteinDataset:
+def load_dataset(path: Path, 
+                 train_root: Path = PathConfig.train_dir, 
+                 external_root: Path = PathConfig.external_dir, 
+                 transform=None) -> ProteinDataset:
     state = torch.load(path, weights_only=False)
     ds = ProteinDataset(
         image_ids=state['image_ids'],
         labels=state['labels'],
-        train_root=state['train_root'],
-        external_root=state['external_root'],
+        train_root=train_root,
+        external_root=external_root,
         transform=transform or state['transform']
     )
     return ds
