11e9ed503978605de0eb7022d92b6d580335489b
dmytro <dmytro19022006@gmail.com>
2026-04-23 19:23:34 +0300

    correct config and add device in focal loss

--- config.py
+++ config.py
@@ -11,7 +11,6 @@ class PathConfig:
     train_dir = data_dir / "train"
     external_dir =  data_dir / "external"
     test_dir = data_dir / "test"
-    npy_cache_dir = data_dir / "npy_cache"
 
     train_labels_path = data_dir / "train.csv"
     external_labels_path = data_dir / "external.csv"
@@ -29,7 +28,6 @@ class PathConfig:
     train_dir.mkdir(parents=True, exist_ok=True)
     external_dir.mkdir(parents=True, exist_ok=True)
     test_dir.mkdir(parents=True, exist_ok=True)
-    npy_cache_dir.mkdir(parents=True, exist_ok=True)
     checkpoints_dir.mkdir(parents=True, exist_ok=True)
     submission_dir.mkdir(parents=True, exist_ok=True)
     saved_datasets_dir.mkdir(parents=True, exist_ok=True)
@@ -38,17 +36,17 @@ class PathConfig:
 
 class DataConfig:
     val_fraction = 0.15
-    train_batch = 64
-    valid_batch = 32
-    test_batch = 32
+    train_batch = 32
+    valid_batch = 16
+    test_batch = 16
 
     input_dim = 512
     input_ch = 4
     n_classes = 28
 
-    n_workers = 8
-    persistent_workers = True
-    prefetch_factor = 4
+    n_workers = 4
+    persistent_workers = n_workers > 0
+    prefetch_factor = 2
 
 class TrainConfig:
     seed = 275
@@ -56,9 +54,9 @@ class TrainConfig:
     classifier_lr = 0.001
 
     epochs = 20
-    early_stop = 5
+    early_stop = 4
     max_norm = 1.5
-    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    device = torch.device('cuda')
  
     model = {
         "name": "efficientnet_v2",
@@ -83,8 +81,8 @@ class TrainConfig:
         "parameters": {
             "alphas": torch.tensor([0.00899, 0.09245, 0.03203, 0.07371, 0.06241, 0.0461, 0.11505, 0.04105, 2.20648, 2.54594, 4.13715, 
                                     0.10608, 0.16829, 0.21585, 0.10887, 5.5162, 0.21822, 0.55162, 0.12795, 0.07837, 0.67545, 0.03076, 
-                                    0.14432, 0.03898, 0.36106, 0.01409, 0.35335, 9.92916], dtype=float, device=device),
-            "gamma": 2.5,
+                                    0.14432, 0.03898, 0.36106, 0.01409, 0.35335, 9.92916], dtype=float),
+            "gamma": 2,
             "reduction": "mean",
             "device": device
         },
@@ -93,7 +91,7 @@ class TrainConfig:
         "name": "reduce_lr_on_plateau",
         "parameters": {
             "mode": "min", 
-            "factor": 0.5, 
+            "factor": 0.65, 
             "patience": 4,
             "threshold": 0.001, 
             "threshold_mode": "rel",
